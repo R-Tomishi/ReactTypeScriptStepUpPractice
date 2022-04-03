c6f575d1cdd2021edf5cfbd310cde348de601306
R-Tomishi <43924379+R-Tomishi@users.noreply.github.com>
2022-04-03 21:40:09 +0900

    3

--- src/Todo.jsx
+++
@@ -1,6 +0,0 @@
-export const Todo = (props) => {
-  const { title, userid, completed } = props;
-  const completedMark = completed ? "[完]" : "[未]";
-
-  return <p>{`${completedMark}${title}(ユーザー:${userid})`}</p>;
-};
